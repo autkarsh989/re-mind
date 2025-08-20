e9c25cc87d4ff92cdbe77f193480224f29807a84
autkarsh989 <autkarsh989@gmail.com>
2025-08-20 23:49:13 +0530

    initial commit

---
+++ src/main/java/com/project/re_mind/ReMindApplication.java
@@ -0,0 +1,13 @@
+package com.project.re_mind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/re_mind/ReMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.re_mind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
